9b55be48ca1004b89c4621c84cbdba54cc65c9f9
Damir <salkynbekov.damir@gmail.com>
2025-01-18 18:35:24 +0500

    Добавлены модели для таблиц

--- rental_management/core/settings.py
+++ rental_management/core/settings.py
@@ -76,14 +76,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'rental_db',
+    #     'USER': 'admin',
+    #     'PASSWORD': 'root',
+    #     'HOST': 'db',
+    #     'PORT': '5432',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rental_db',
-        'USER': 'admin',
-        'PASSWORD': 'root',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
 }
 
 
--- rental_management/rental/models.py
+++ rental_management/rental/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255,
+                            unique=True,
+                            verbose_name="Название продукта")
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2,
+                                verbose_name="Цена продукта")
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    start_rental = models.DateField(verbose_name="Начало аренды")
+    end_rental = models.DateField(verbose_name="Конец аренды")
+    total_price = models.DecimalField(max_digits=10,
+                                      decimal_places=2,
+                                      verbose_name="Общая стоимость",
+                                      default=None)
+
+    def __str__(self):
+        return f"Заказ #{self.id} ({self.start_rental} - {self.end_rental})"
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order,
+                              on_delete=models.CASCADE,
+                              related_name="order_products",
+                              verbose_name="Заказ")
+    product = models.ForeignKey(Product,
+                                on_delete=models.CASCADE,
+                                related_name="order_products",
+                                verbose_name="Продукт")
+    product_price = models.DecimalField(max_digits=10,
+                                        decimal_places=2,
+                                        verbose_name="Цена продукта в аренде")
+    duration = models.PositiveIntegerField(verbose_name="Продолжительность аренды")
+
+    def __str__(self):
+        return f"{self.product.name} в заказе #{self.order.id}"
