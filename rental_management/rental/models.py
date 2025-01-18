bbd0e7a5111730cebb666fcf72d6076dbadf21a5
Damir <salkynbekov.damir@gmail.com>
2025-01-18 18:41:14 +0500

    Добавлены валидации согласно условиям

--- rental_management/rental/models.py
+++ rental_management/rental/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -21,6 +22,14 @@ class Order(models.Model):
                                       verbose_name="Общая стоимость",
                                       default=None)
 
+    def clean(self):
+        if self.start_rental >= self.end_rental:
+            raise ValidationError("Дата конца аренды должна быть после даты начала аренды")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"Заказ #{self.id} ({self.start_rental} - {self.end_rental})"
 
@@ -38,6 +47,23 @@ class OrderProduct(models.Model):
                                         decimal_places=2,
                                         verbose_name="Цена продукта в аренде")
     duration = models.PositiveIntegerField(verbose_name="Продолжительность аренды")
+
+    def clean(self):
+        overlapping_orders = OrderProduct.objects.filter(
+            product=self.product,
+            order__start_rental__lt=self.order.end_rental,
+            order__end_rental__gt=self.order.start_rental
+        ).exclude(order=self.order)
+
+        if overlapping_orders.exists():
+            raise ValidationError(f"Продукт {self.product.name} уже в арендован на определенный срок")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        unique_together = ('order', 'product')
 
     def __str__(self):
         return f"{self.product.name} в заказе #{self.order.id}"
